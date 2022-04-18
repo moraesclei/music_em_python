9f52c0fed17c52aa1c0faef618c4824990d59c0c
Moraes Clei <moraesclei@gmail.com>
2022-04-18 08:23:44 -0300

    import de um áudio em mp3

--- impormusica.py
+++ impormusica.py
@@ -1,3 +1,4 @@
+# Primeiro Projeto em python a qual conseguimos importar um áudio em mp3 e executá-l0
 # o codigo a seguir faz tocar uma música a partir dos comandos abaixo:
 # neste caso precisamos instalar primeiro o pacote pygame e só depois realizar o import.
 import pygame
